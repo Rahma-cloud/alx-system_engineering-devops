de59b696442b988348a70ae7e909185092216d05
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-08-04 09:13:58 -0700

    tasks

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
 # 100-textme.rb script
-#puts ARGV[0].scan(/\[from:([\w+]+)\] \[to:([\w+]+)\] \[flags:([\d\-:]+)\]/).join(",")
+puts ARGV[0].scan(/\[from:([\w+]+)\] \[to:
+([\w+]+)\] \[flags:([\d\-:]+)\]/).join(",")
