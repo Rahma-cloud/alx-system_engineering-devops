eff5d2e6633dc54a2e140ab7c6b681edd0e7514e
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-09-22 09:26:51 -0700

    tasks

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,9 @@
 #this is for my last task
 exec { 'kill_killmenow_process':
-  command => 'pkill killme now',
-  onlyif  => 'pgreo killmenow',
+  command     => 'pkill killme now',
+  refreshonly => true,
+}
+service { 'my_service':
+  ensure  => 'running',
+  require => Exec['killmenow'],
 }
