01e4a786c24b980493b91506eceb2533189c2558
Rahma-cloud <folorunshorahmat16@gmail.com>
2023-09-22 08:36:57 -0700

    tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
-#this is to install flask frpm pip3
+#this is to install python from pip3
 package { 'python3-pip':
-  ensure => installed,
+  ensure     => installed,
 }
+#this is to install flask from pip3
 exec { 'install flask':
-  command    => '/usr/bin/pip3 install flask==2.1.0',
-  path       => ['/usr/bin', '/usr/local/bin'],
-  unless     => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
