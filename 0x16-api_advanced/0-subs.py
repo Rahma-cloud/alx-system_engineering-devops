94cbb85690b92f67efb6330b5bf6c85ce76dc247
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2023-11-07 14:57:12 +0100

    Api tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subcribers"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'my-app/0.0.1'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
     try:
         response = requests.get(url, headers=headers)
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Task 1"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """return the titles of ten hot posts"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+        else:
+            print(None)
+    except requests.exceptions.HTTPError as e:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
